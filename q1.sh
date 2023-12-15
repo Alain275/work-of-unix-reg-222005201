0f5470ede4d17dfe95e5114fb0ca8283ffe3c80c
Alain <142985170+Alain275@users.noreply.github.com>
2023-12-15 07:58:44 +0200

    com

--- q1.sh
+++ q1.sh
@@ -5,7 +5,7 @@ Solution 1
 if [ $# -ne 3 ]; then
     echo "Error: The script requires exactly 3 arguments." > error.txt
     exit 1
-fi
+fi  
 
 # If the number of arguments is correct, sort the numbers in ascending order and write them to a file
 echo -e "$1\n$2\n$3" | sort -n > numbers.txt     alain
